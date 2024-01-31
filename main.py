1c9a4b2efbf4fbbe95846274bf9e1e6a5706b39e
andmansim <91722324+andmansim@users.noreply.github.com>
2024-01-31 12:00:52 +0100

    PUTA VIDA

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
 
-df_equipos = pd.read_csv('equipo.csv')
-df_partidos = pd.read_csv('partidos.csv')
+def leer_csv(ruta, indice):
+    with open (ruta, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        df = pd.DataFrame(reader, columns = indice)
+    return df
 
-df_equipos.head()
+df_equipo = leer_csv('equipo.csv', ['nombre', 'img', 'pais', 'numero'])
+df_partidos = leer_csv('partidos.csv', ['fecha', 'hora', 'equipo1', 'equipo2', 'goles1', 'goles2'])#NO TENGO NI ZORRA DE QUE COÑO VA ESTE PUTO CSV
+

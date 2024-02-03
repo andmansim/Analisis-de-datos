5e7ae03fb3ad8443f502bb61ed95bcae5869534c
andmansim <91722324+andmansim@users.noreply.github.com>
2024-02-03 21:07:45 +0100

    me canse

--- main2.py
+++ main2.py
@@ -16,6 +16,12 @@ print(df_ganador.info())
 print(df_ganador.columns)
 #No hay ningún valor nulo
 
+def representar_barras(datos, datos2):
+    plt.bar(datos, datos2)
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(left=0.1, bottom=0.3, right=0.9, top=0.9)
+    plt.show()
+
 print('\n'+ 'Representaciones gráficas:')
 #Agrupamos por team(Equipo que metió gol en dicho minuto) y hacemos la media del minuto
 df_goles_media = df_goles.groupby('team').mean()
@@ -24,11 +30,7 @@ df_goles_media = df_goles.groupby('team').mean()
 df_goles_media = df_goles_media.sort_values(by='minute', ascending=False)
 df_goles_media = df_goles_media.head(15)
 print(df_goles_media)
-
-plt.bar(df_goles_media.index, df_goles_media['minute'])
-plt.xticks(rotation=45, ha='right')
-plt.subplots_adjust(left=0.1, bottom=0.3, right=0.9, top=0.9)
-plt.show()
+representar_barras(df_goles_media.index, df_goles_media['minute'])
 
 
 #Agrupamos y contamos las veces que han jugado en casa y fuera
@@ -38,17 +40,11 @@ df_ganadoraway = df_ganador['away_team'].value_counts()
 #Representamos los 15 equipos con mayor media de goles en casa y fuera
 df_ganadorhome = df_ganadorhome.sort_values(ascending=False)
 df_ganadorhome = df_ganadorhome.head(15)
-plt.bar(df_ganadorhome.index, df_ganadorhome)
-plt.xticks(rotation=45, ha='right')
-plt.subplots_adjust(left=0.1, bottom=0.3, right=0.9, top=0.9)
-plt.show()
+representar_barras(df_ganadorhome.index, df_ganadorhome)
 
 df_ganadoraway = df_ganadoraway.sort_values(ascending=False)
 df_ganadoraway = df_ganadoraway.head(15)
-plt.bar(df_ganadoraway.index, df_ganadoraway)
-plt.xticks(rotation=45, ha='right')
-plt.subplots_adjust(left=0.1, bottom=0.3, right=0.9, top=0.9)
-plt.show()
+representar_barras(df_ganadoraway.index, df_ganadoraway)
 
 
 
@@ -58,7 +54,7 @@ df_ganador = df_ganador['winner'].value_counts()
 print(df_ganador)
 
 #Contamos cuantas veces ha marcado gol el jugador 
-
+print('\n'+ 'Ranking de goles de cada jugador:')
 df_golesjugadores = df_goles['scorer'].value_counts()
 print(df_golesjugadores)
 

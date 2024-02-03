88c302aaf357ec774d547159b5e8eab76a531a64
andmansim <91722324+andmansim@users.noreply.github.com>
2024-02-03 20:26:20 +0100

    PUTA VIDA

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
 
-df_equipos = pd.read_csv('equipo.csv')
-df_partidos = pd.read_csv('partidos.csv')
+def leer_csv(ruta, indice):
+    #Función encargada en leer y asignar los nombres de las columnas correspondientes
+    with open (ruta, 'r', encoding='utf-8') as f:
+        reader = csv.reader(f, delimiter=',')
+        df = pd.DataFrame(reader, columns = indice)
+    return df
 
-df_equipos.head()
+#Leemos cada csv y creamos los DataFrames correspondientes
+df_equipo = leer_csv('equipo.csv', ['nombre', 'img', 'pais', 'numero'])
+df_partidos = leer_csv('partidos.csv', ['dia', 'equipo1', 'equipo2', 'numero1', 'numero2', 'numero3'])
+
+#Vemos la información de cada DataFrame
+print(df_equipo.info())
+print(df_partidos.info())
+#No hay ningún valor nulo
+
+#transformamos las columnas de numero a enteros
+df_equipo['numero'] = df_equipo['numero'].astype(int)
+
+#Ordenamos los equipos por numero
+df_equipo = df_equipo.sort_values(by='numero', ascending=True)
+print(df_equipo.head())
+
+# Representación de los equipos por país con nombres rotados
+plt.bar(df_equipo['pais'].value_counts().index, df_equipo['pais'].value_counts())
+plt.xticks(rotation=45, ha='right')  # Ajusta la rotación y alineación horizontal de los nombres
+plt.xlabel('País')
+plt.ylabel('Número de Equipos')
+plt.title('Número de Equipos por País')
+plt.tight_layout()
+plt.show()
---
+++ main2.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+df_goles = pd.read_csv('goalscorers.csv', encoding='utf-8', delimiter=',')
+df_ganador = pd.read_csv('winner.csv', encoding='utf-8', delimiter=',')
+
+#Vemos la información de cada DataFrame
+print(df_goles.info())
+print(df_ganador.info())
+#No hay ningún valor nulo
+
+#Agrupamos por team y hacemos la media del minuto
+df_goles_media = df_goles.groupby('team').mean()
+print(df_goles_media)
+
+#Agrupamos y contamos las veces que han jugado en casa y fuera
+df_ganadorhome= df_ganador['home_team'].value_counts()
+df_ganadoraway = df_ganador['away_team'].value_counts()
+print(df_ganadorhome)
+print(df_ganadoraway)
+
+#Contamos las veces que ha ganado cada equipo
+df_ganador = df_ganador['winner'].value_counts()
+print(df_ganador)
+
+#Contamos cuantas veces ha marcado gol el jugador 
+print(df_goles.columns)
+df_golesjugadores = df_goles['scorer'].value_counts()
+print(df_golesjugadores)
+
